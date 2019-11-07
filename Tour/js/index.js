be205281cff61a1363fe1c96fa0ea00cfcd62b36
wwang-luse <wwangtaoc11@163.com>
2019-11-07 17:26:14 +0800

    tour相关

--- Tour/js/index.js
+++ Tour/js/index.js
@@ -7,4 +7,22 @@ $(function(){
 		$(this).removeClass("open");
 		$(".topNavLanguage .second-menu").css("display","none");
 	});
+	
+	$("#menuNav li:not(li:first)").hover(function(){
+		$("#menuNav li").find(".submenu-nav").css("display","none");		
+		var len=$(this).find(".submenu-nav").length;		
+		if(len>0){
+			$("#submenuNavBg").css("display","none");			
+			$(this).find(".submenu-nav").css("display","block");			
+		}else{
+			$("#submenuNavBg").css("display","block");	
+		}
+	  	$(this).addClass("open");
+		
+		
+	});
+	$("#menuNav li").mouseleave(function(){
+		$(this).find("submenu-nav").css("display","none");
+		$(this).removeClass("open");
+	});
 })
