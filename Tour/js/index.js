70fd7b1ef23f22a3a2d0b05b670ee78a08591ba4
wwang-luse <wwangtaoc11@163.com>
2019-11-08 13:42:28 +0800

    tour相关

--- Tour/js/index.js
+++ Tour/js/index.js
@@ -8,7 +8,7 @@ $(function(){
 		$(".topNavLanguage .second-menu").css("display","none");
 	});
 	
-	$("#menuNav li:not(li:first)").hover(function(){
+	$("#menuNav li").hover(function(){
 		$("#menuNav li").find(".submenu-nav").css("display","none");		
 		var len=$(this).find(".submenu-nav").length;		
 		if(len>0){
@@ -16,7 +16,7 @@ $(function(){
 			$(this).find(".submenu-nav").css("display","block");			
 		}else{
 			$("#submenuNavBg").css("display","block");	
-		}
+		}		
 	  	$(this).addClass("open");
 		
 		
@@ -24,5 +24,20 @@ $(function(){
 	$("#menuNav li").mouseleave(function(){
 		$(this).find("submenu-nav").css("display","none");
 		$(this).removeClass("open");
+	});
+	
+	$(".rcc-ban li").click(function(){
+		$(".rcc-ban li").removeClass("rcc-at");
+		$(this).addClass("rcc-at");
+		var index=$(".rcc-ban li").index(this);		
+		$(".recom-city .rcc-con").css("display","none");
+	    $(".recom-city .rcc-con").eq(index).css("display","block");	
+		
+	});
+	$(".nowCity").hover(function(){
+		$(this).addClass("active");
+	});
+	$(".nowCity").mouseleave(function(){
+		//$(this).removeClass("active");
 	});
 })
